6e2230d01792347a58cf2f0622761383e31ec36a
bkmfree <bkmfree@naver.com>
2026-09-12 12:43:37 +0000

    npm test 3*5 = 12

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('3 곱하기 4는 15가 나와야 해요', () => {
-  expect(3 * 4).toBe(12);
+test('3 곱하기 5는 15가 나와야 해요', () => {
+  expect(3 * 5).toBe(15);
 });
